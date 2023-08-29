62e6f2b30d45605c0520570c2121fea2bb394f1c
jiangyinzuo <jiangyinzuo@foxmail.com>
2023-08-29 20:24:23 +0800

    rename result of make-tar/cpp.sh

--- make-tar/cpp.sh
+++ make-tar/cpp.sh
@@ -8,6 +8,6 @@ fi
 
 pushd $plugged_dir
 
-tar -cf plugged.tar coc-fzf fzf tabular vim-cpp-modern vim-markdown vim-test LeaderF coc.nvim fzf.vim tagbar vim-oscyank any-jump.vim traces.vim vim-floaterm vim-sneak asyncrun.vim nordtheme ultisnips vim-fugitive vim-snippets asynctasks.vim dracula vim-gitgutter vim-solarized8 vista.vim auto-pairs editorconfig-vim vim-code-dark vim-go vim-surround
+tar -cf plugged-cpp.tar coc-fzf fzf tabular vim-cpp-modern vim-markdown vim-test LeaderF coc.nvim fzf.vim tagbar vim-oscyank any-jump.vim traces.vim vim-floaterm vim-sneak asyncrun.vim nordtheme ultisnips vim-fugitive vim-snippets asynctasks.vim dracula vim-gitgutter vim-solarized8 vista.vim auto-pairs editorconfig-vim vim-code-dark vim-go vim-surround
 
 popd
