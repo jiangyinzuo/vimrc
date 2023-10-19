a9dc818ecf752e7291d49e5ae2e9effad1f81cf3
jiangyinzuo <jiangyinzuo@foxmail.com>
2023-10-19 12:31:14 +0800

    add proxy for WSL2

--- root/wsl-init.bash
+++ root/wsl-init.bash
@@ -8,3 +8,27 @@ export CODE_HOME=~
 # echo ''
 
 alias daily='vim $DOC2/daily'
+
+######################### Clash Proxy ########################
+clashproxy() {
+	if [[ $1 == 'on' ]]; then
+		# Clash for Windows, WSL2走Clash代理
+		# 配置Clash: 找到General > Allow LAN，打开开关。
+		# https://wph.im/199.html
+		hostip=$(cat /etc/resolv.conf |grep -oP '(?<=nameserver\ ).*')
+		( 
+			set -x
+			export https_proxy="http://${hostip}:7890"
+			export http_proxy="http://${hostip}:7890"
+			export all_proxy="socks5://${hostip}:7890"
+		)
+	else
+		(
+			set -x
+			unset https_proxy
+			unset http_proxy
+			unset all_proxy
+		)
+	fi
+}
+##############################################################
