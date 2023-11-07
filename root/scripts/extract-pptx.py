b4cc0854dbedb85255a7ed92ecb0cb2d59c654dd
jiangyinzuo <jiangyinzuo@foxmail.com>
2023-11-07 14:14:52 +0800

    feat: clangd config.yaml, extract-pptx.py

--- root/scripts/extract-pptx.py
+++ root/scripts/extract-pptx.py
@@ -1,21 +1,28 @@
 #!/bin/python3
 
 from pptx import Presentation
-from pptx.enum.shapes import MSO_SHAPE_TYPE
+from pptx.text.text import _Paragraph
+from pptx.shapes.placeholder import SlidePlaceholder
 from pptx.enum.shapes import PP_PLACEHOLDER
 
 
-# 定义一个函数，用于打印带缩进和bullet的段落文本
-def _print_indented_text(paragraph):
+def _print_indented_text(paragraph: _Paragraph, bullet: str):
+    """
+    定义一个函数，用于打印带缩进和bullet的段落文本
+    """
     if len(paragraph.text) == 0:
         return
     indent_level = paragraph.level
-    # 根据缩进级别添加制表符
-    indent = '\t' * indent_level
-    # 添加bullet或其他符号，如果有的话
-    bullet_symbol = '- ' if indent_level > 0 else ''
+    # 根据缩进级别添加空格
+    indent = '    ' * indent_level
     # 打印缩进和bullet后的段落文本
-    print(f"{indent}{bullet_symbol}{paragraph.text}")
+    print(f"{indent}{bullet} ", end='')
+    for run in paragraph.runs:
+        if run.font.bold:
+            print(f" **{run.text.strip()}** ", end='')
+        else:
+            print(run.text, end='')
+    print()
 
 
 def extract_pptx(pptx_file: str):
@@ -23,12 +30,21 @@ def extract_pptx(pptx_file: str):
     for i, slide in enumerate(prs.slides):
         print(f"=== Slide {i + 1}\n")
         for shape in slide.shapes:
-            if hasattr(shape, "text"):
-                # 遍历文本框中的每一个段落
-                for paragraph in shape.text_frame.paragraphs:
-                    _print_indented_text(paragraph)
-                    pass
-                print()
+            if shape.is_placeholder:
+                shape: SlidePlaceholder
+                if hasattr(shape, "text") and len(shape.text.strip()) > 0:
+                    bullet = '-'
+                    match shape.placeholder_format.type:
+                        case PP_PLACEHOLDER.TITLE:
+                            bullet=''
+                            print("##", end="")
+                        case PP_PLACEHOLDER.SUBTITLE:
+                            bullet=''
+                    # 遍历文本框中的每一个段落
+                    for paragraph in shape.text_frame.paragraphs:
+                        _print_indented_text(paragraph, bullet)
+                        pass
+                    print()
 
 
 if __name__ == '__main__':
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 soft_link_files=(
 	.vim .ripgreprc .globalrc .tmux.conf .config/ctags .config/lazygit
-	.config/vifm .gitconfig .gitconfig-ict .config/himalaya
+	.config/vifm .gitconfig .gitconfig-ict .config/himalaya .config/clangd
 )
 
 if [[ `uname -a` == *WSL* ]]; then
