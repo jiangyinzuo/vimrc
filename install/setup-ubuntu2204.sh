7af85071deccc0e62201819993323f0591d367c5
jiangyinzuo <jiangyinzuo@foxmail.com>
2023-11-27 14:22:02 +0800

    feat: dictionary completion

--- install/setup-ubuntu2204.sh
+++ install/setup-ubuntu2204.sh
@@ -13,7 +13,10 @@ set -v
 
 # apt install -y golang clangd
 # Leaderf needs python3-dev and python3-distutils
-apt-get install -y vim-gtk3 ripgrep fzf fd-find tree bat nodejs npm git sqlformat vifm python3-dev python3-distutils
+# wamerican: American English字典文件，安装后位于/usr/share/dict/american-english, 用于vim dictionary
+apt-get install -y vim-gtk3 ripgrep fzf fd-find tree bat nodejs npm git sqlformat vifm python3-dev python3-distutils \
+	wamerican
+
 # ripgrep-all（master分支）
 # See: https://github.com/phiresky/ripgrep-all/issues/113
 # apt install ripgrep pandoc poppler-utils ffmpeg
