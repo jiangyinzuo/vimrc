ec970562139aefa3e498a4892a21861e3c41d062
jiangyinzuo <jiangyinzuo@foxmail.com>
2024-01-13 20:58:27 +0800

    chore: rsync-vimrc.sh use git worktree

--- tools/rsync-vimrc.sh
+++ tools/rsync-vimrc.sh
@@ -6,6 +6,17 @@ _help() {
 	exit 1
 }
 
+_git_worktree_origin_dir() {
+	gitdir=$(git rev-parse --git-dir)
+	if [[ "$gitdir" == '.git' ]]; then
+		echo "not in worktree"
+		exit 1
+	fi
+	# /root/vimrc/.git/worktrees/vimrc-kiwi -> /root/vimrc
+	# 去掉.git/worktrees及其后面的内容
+	origin_dir=$(echo $gitdir | sed 's/\.git\/worktrees\/.*//')
+}
+
 host=$1
 if [ -z "$host" ]; then
 	_help
@@ -21,26 +32,35 @@ case $1 in
 		;;
 esac
 
+# 获取当前脚本所在git仓库的根目录
+worktree_dir=$(git rev-parse --show-toplevel)
+worktree_basename=$(basename $worktree_dir)
+echo "worktree_dir: $worktree_dir"
+cd $worktree_dir
+_git_worktree_origin_dir
+echo "origin_dir: $origin_dir"
+
 case $direction in
 	"to")
 		# 在远程主机上执行checkrepo.py脚本，若返回非0值，则退出
-		ssh $host "python3 ~/vimrc/root/scripts/checkrepo.py ~/vimrc"
+		ssh $host "python3 ~/vimrc/root/scripts/checkrepo.py safersync --repo $worktree_dir"
 		# 如果没有指定-f参数，且返回非0值，则退出
 		if [[ $? -ne 0 && "$force" != '-f' ]]; then
 			echo -e "\033[31mcheckrepo.py failed\033[0m"
 			exit 1
 		fi
-		rsync-git . $host:~/vimrc
+		rsync-git $worktree_dir/ $host:~/$worktree_basename
+		rsync-git $origin_dir $host:~/vimrc
 		;;
 	"from")
 		# 在本地主机上执行checkrepo.py脚本，若返回非0值，则退出
-		python3 ~/vimrc/root/scripts/checkrepo.py ~/vimrc
+		python3 ~/vimrc/root/scripts/checkrepo.py safersync --repo $worktree_dir
 		# 如果没有指定-f参数，且返回非0值，则退出
 		if [[ $? -ne 0 && "$force" != '-f' ]]; then
 			echo -e "\033[31mcheckrepo.py failed\033[0m"
 			exit 1
 		fi
-		rsync-git $host:~/vimrc ~
+		rsync-git $host:~/$worktree_basename/ $worktree_dir/
 		;;
 	*)
 		_help
