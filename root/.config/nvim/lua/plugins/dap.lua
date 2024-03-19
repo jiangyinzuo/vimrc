2f1f88d6984e43afbae338bafe690e842248fa5f
jiangyinzuo <jiangyinzuo@foxmail.com>
2024-03-19 11:51:01 +0800

    fix(nvim): dependencies of nvim-dap-ui, codeium.vim

--- root/.config/nvim/lua/plugins/dap.lua
+++ root/.config/nvim/lua/plugins/dap.lua
@@ -2,13 +2,24 @@ if vim.g.vim_dap == 'nvim-dap' then
 	return {
 		{
 			'rcarriga/nvim-dap-ui',
+			lazy = true,
+			cmd = {
+				"DapContinue",
+				"DapToggleRepl",
+				"DapSetLogLevel",
+				"DapLoadLaunchJSON",
+				"DapToggleBreakpoint",
+			},
 			dependencies = {
 				'mfussenegger/nvim-dap',
+				"nvim-neotest/nvim-nio",
+				'theHamsta/nvim-dap-virtual-text',
 			},
 			config = require('dapconfig').dapconfig
 		},
 		{
 			'theHamsta/nvim-dap-virtual-text',
+			lazy = true,
 			opts = {
 				commented = true
 			}
--- root/.config/nvim/lua/plugins/init.lua
+++ root/.config/nvim/lua/plugins/init.lua
@@ -1,5 +1,42 @@
 local plugins_setup = require('plugins_setup')
 
+local ai_complete
+if vim.g.ai_complete == 'codeium' then
+	ai_complete = 'Exafunction/codeium.vim'
+else
+	ai_complete = {
+		"CopilotC-Nvim/CopilotChat.nvim",
+		lazy = true,
+		branch = "canary",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		cmd = {
+			"CopilotChat",
+			"CopilotChatFix",
+			"CopilotChatDocs",
+			"CopilotChatLoad",
+			"CopilotChatOpen",
+			"CopilotChatSave",
+			"CopilotChatClose",
+			"CopilotChatReset",
+			"CopilotChatTests",
+			"CopilotChatCommit",
+			"CopilotChatToggle",
+			"CopilotChatExplain",
+			"CopilotChatOptimize",
+			"CopilotChatDebugInfo",
+			"CopilotChatCommitStaged",
+			"CopilotChatFixDiagnostic",
+		},
+		opts = {
+			debug = true, -- Enable debugging
+			-- See Configuration section for rest
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	}
+end
 return {
 	{
 		"ishan9299/nvim-solarized-lua",
@@ -38,19 +75,7 @@ return {
 		"williamboman/mason.nvim",
 		config = plugins_setup.mason,
 	},
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "canary",
-		dependencies = {
-			{ "github/copilot.vim" }, -- or github/copilot.vim
-			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-		},
-		opts = {
-			debug = true, -- Enable debugging
-			-- See Configuration section for rest
-		},
-		-- See Commands section for default commands if you want to lazy load on them
-	},
+	ai_complete,
 	{
 		'sindrets/diffview.nvim',
 	},
