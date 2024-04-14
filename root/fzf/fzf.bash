44d83ac829c6dd08d7933846f897b187648ddedb
jiangyinzuo <jiangyinzuo@foxmail.com>
2024-04-14 17:01:02 +0800

    fix(scripts): use batcat instead of bat

--- root/fzf/fzf.bash
+++ root/fzf/fzf.bash
@@ -82,7 +82,7 @@ rfv() {
 		--prompt '1. ripgrep> ' \
 		--delimiter : \
 		--header 'CTRL-T: Switch between ripgrep/fzf' \
-		--preview 'bat --color=always {1} --highlight-line {2}' \
+		--preview 'batcat --color=always {1} --highlight-line {2}' \
 		--preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
 		--bind 'enter:become(vim {1} +{2})'
 }
