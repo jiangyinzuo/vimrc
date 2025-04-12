5def3eea7daf88e65eee9ebb3a72b467b879c6b4
Yinzuo Jiang <jiangyinzuo@foxmail.com>
2025-04-12 14:56:30 +0800

    feat: mcphub.nvim

--- root/.config/nvim/lua/plugins/ai.lua
+++ root/.config/nvim/lua/plugins/ai.lua
@@ -195,6 +195,20 @@ return {
 		cond = vim.g.ai_suggestion == "minuet-ai.nvim",
 	},
 	{
+		"ravitemer/mcphub.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
+		},
+		-- comment the following line to ensure hub will be ready at the earliest
+		cmd = "MCPHub", -- lazy load by default
+		build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+		-- uncomment this if you don't want mcp-hub to be available globally or can't use -g
+		-- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+		config = function()
+			require("mcphub").setup()
+		end,
+	},
+	{
 		"yetone/avante.nvim",
 		event = "VeryLazy",
 		lazy = true,
