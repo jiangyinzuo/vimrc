acef7117c64ea92968a7111a9df796c05d7c31fb
Yinzuo Jiang <jiangyinzuo@foxmail.com>
2025-05-25 16:17:32 +0800

    feat(treesitter): specify main branch for nvim-treesitter plugin

--- root/.config/nvim/lua/plugins/treesitter.lua
+++ root/.config/nvim/lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
+		branch = "main",
 		config = function()
 			require("nvim-treesitter.configs").setup({
 				-- 安装 language parser
