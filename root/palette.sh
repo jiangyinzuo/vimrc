4e808c2136b36fa3f83c7d942583b6eeb44f3422
Yinzuo Jiang <jiangyinzuo@foxmail.com>
2025-06-04 18:16:22 +0800

    refactor: remove palette.sh

--- root/palette.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rg -l '[[palette]]' ~/.vim/after  ~/.vim/ftplugin ~/.vim/plugin ~/.vim/vimrc.d ~/.vim/vimrc | xargs sed -n 's/.*\[\[palette\]\]\(.*\)/\1/p'
-cat ~/.vim/doc/palette.txt
