ce94789b24124a846c000a8ed06aa81bcdebf8f7
Yinzuo Jiang <jiangyinzuo@foxmail.com>
2025-07-07 22:34:32 +0800

    refactor: move lualine config to plugin file and update configs
    - Extract lualine setup from lazy.lua to dedicated plugins/lualine.lua
    - Add nvim-web-devicons as dependency
    - Update AI-related config examples in config.vim.example
    - Adjust temperature setting in vim-ai-roles.ini

--- root/.config/nvim/lazy.lua
+++ root/.config/nvim/lazy.lua
@@ -30,101 +30,6 @@ require("lazy").setup("plugins", {
 	},
 })
 
-local function setup_lualine()
-	local lualine_b = {}
-	local lualine_c = {}
-	local winbar = {}
-	if vim.g.vimrc_lsp == "nvim-lsp" then
-		lualine_b = { "branch" }
-		lualine_c = {
-			"diagnostics",
-			-- invoke `progress` to get lsp progress status.
-			require("lsp-progress").progress,
-		}
-		winbar = {
-			lualine_c = {
-				"navic",
-				color_correction = nil,
-				navic_opts = nil,
-			},
-		}
-	elseif vim.g.vimrc_lsp == "coc.nvim" then
-		lualine_b = { "branch" }
-		lualine_c = {
-			-- invoke `coc#status` to get coc status.
-			[[%{exists("*coc#status")?coc#status():''}]],
-		}
-		winbar = {
-			lualine_c = {
-				[[%{%get(b:, 'coc_symbol_line', '')%}]],
-			},
-		}
-	end
-
-	local filename = {
-		"filename",
-		path = 4,
-		shorting_target = 40,
-	}
-	require("lualine").setup({
-		options = {
-			icons_enabled = true,
-			theme = "auto",
-			component_separators = { left = "", right = "" },
-			-- leave them blank, or lualine will kill intro.
-			-- https://github.com/nvim-lualine/lualine.nvim/issues/259#issuecomment-1890485361
-			section_separators = { left = "", right = "" },
-			disabled_filetypes = {
-				winbar = {
-					-- do not override dapui's buttons
-					"dap-repl",
-					"dapui_breakpoints",
-					"dapui_console",
-					"dapui_scopes",
-					"dapui_watches",
-					"dapui_stacks",
-					"termdebug",
-					"Avante",
-					"AvanteSelectedFiles",
-					"AvanteInput",
-					"oil",
-				},
-			},
-			ignore_focus = {},
-			always_divide_middle = true,
-			globalstatus = false,
-			refresh = {
-				statusline = 1200,
-				tabline = 1000,
-				winbar = 1200,
-			},
-		},
-		sections = {
-			lualine_a = { filename },
-			-- 'diff' is slow
-			lualine_b = lualine_b,
-			lualine_c = lualine_c,
-			lualine_x = {},
-			lualine_y = {},
-			-- file location
-			lualine_z = { "%l/%L:%v", { "filetype", icons_enabled = false }, "encoding", "fileformat" },
-		},
-		inactive_sections = {
-			lualine_a = {},
-			lualine_b = {},
-			lualine_c = { filename },
-			-- file location
-			lualine_x = { "%l/%L:%v" },
-			lualine_y = {},
-			lualine_z = {},
-		},
-		tabline = {},
-		winbar = winbar,
-		inactive_winbar = {},
-		extensions = { "fern", "quickfix", "nvim-dap-ui" },
-	})
-end
-
 local function config_clipboard()
 	if vim.fn.has('wsl') == 1 then
 		-- clip.exe会导致中文乱码，使用默认的wl-copy
@@ -165,5 +70,4 @@ hi WinBar guibg=NONE
 hi! link CocInlayHint LspInlayHint
 hi link QuickPreview Normal
 ]])
-setup_lualine()
 config_clipboard()
