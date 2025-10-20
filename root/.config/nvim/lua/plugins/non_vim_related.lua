7945417892001b3983d4b0b6bb5da486b9b7bd80
Yinzuo Jiang <jiangyinzuo@foxmail.com>
2025-10-20 15:36:11 +0800

    feat: non-vim-related plugin gdb-bt-forest

---
+++ root/.config/nvim/lua/plugins/non_vim_related.lua
@@ -0,0 +1,9 @@
+--- Using lazy.nvim to manage non Neovim related repositories
+--- https://github.com/folke/lazy.nvim/discussions/1488
+return {
+	{
+		"jiangyinzuo/gdb-bt-forest",
+		lazy = true, -- so neovim doesn't try to load it
+		enabled = true,
+	},
+}
