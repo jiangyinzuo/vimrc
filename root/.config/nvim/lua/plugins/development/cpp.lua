2505ab08b91e9929779d0c58bc28448afcb3dc72
Yinzuo Jiang <jiangyinzuo@foxmail.com>
2025-11-30 14:22:20 +0800

    feat: godbolt.nvim

--- root/.config/nvim/lua/plugins/development/cpp.lua
+++ root/.config/nvim/lua/plugins/development/cpp.lua
@@ -16,8 +16,37 @@ if require("config").load_plugin.development.cpp then
 				skip_input_prompt = true,
 				prefix = "<leader>cs", -- prefix to trigger maps
 				cscope = {
-					exec = vim.g.tag_system
-				}
+					exec = vim.g.tag_system,
+				},
+			},
+		},
+		{
+			"p00f/godbolt.nvim",
+			cond = require("config").load_plugin.public_network,
+			opts = {
+				{
+					languages = {
+						cpp = { compiler = "g122", options = {} },
+						c = { compiler = "cg122", options = {} },
+						rust = { compiler = "r1650", options = {} },
+						-- any_additional_filetype = { compiler = ..., options = ... },
+					},
+					auto_cleanup = true, -- remove highlights and autocommands on buffer close
+					highlight = {
+						cursor = "Visual", -- `cursor = false` to disable
+						-- values in this table can be:
+						-- 1. existing highlight group
+						-- 2. hex color string starting with #
+						static = { "#222222", "#333333", "#444444", "#555555", "#444444", "#333333" },
+						-- `static = false` to disable
+					},
+					-- `highlight = false` to disable highlights
+					quickfix = {
+						enable = false, -- whether to populate the quickfix list in case of errors
+						auto_open = false, -- whether to open the quickfix list in case of errors
+					},
+					url = "https://godbolt.org", -- can be changed to a different godbolt instance
+				},
 			},
 		},
 	}
