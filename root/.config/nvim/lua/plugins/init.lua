dbdaa247e1c1e361e13aa6809fcab1854fa7b524
Yinzuo Jiang <jiangyinzuo@foxmail.com>
2025-12-28 13:59:28 +0800

    fix: nvim-origami config

--- root/.config/nvim/lua/plugins/init.lua
+++ root/.config/nvim/lua/plugins/init.lua
@@ -379,18 +379,6 @@ return {
 		"chrisgrieser/nvim-origami",
 		event = "VeryLazy",
 		opts = {
-			useLspFoldsWithTreesitterFallback = true,
-			pauseFoldsOnSearch = true,
-			foldtext = {
-				enabled = true,
-				padding = 3,
-				lineCount = {
-					template = "%d lines", -- `%d` is replaced with the number of folded lines
-					hlgroup = "Comment",
-				},
-				diagnosticsCount = true, -- uses hlgroups and icons from `vim.diagnostic.config().signs`
-				gitsignsCount = true, -- requires `gitsigns.nvim`
-			},
 			autoFold = {
 				enabled = false,
 				kinds = { "comment", "imports" }, ---@type lsp.FoldingRangeKind[]
