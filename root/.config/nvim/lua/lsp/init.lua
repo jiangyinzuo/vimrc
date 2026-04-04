6b522bb85927e6d814e8143b285b6b8a8449c7d0
Yinzuo Jiang <jiangyinzuo@foxmail.com>
2026-04-04 12:37:40 +0800

    fix: deprecated client.support_method

--- root/.config/nvim/lua/lsp/init.lua
+++ root/.config/nvim/lua/lsp/init.lua
@@ -148,7 +148,7 @@ local function attach_codelens(client, bufnr)
 	-- 来源：知乎
 	-- 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 	-- code lens
-	if client.supports_method("textDocument/codeLens", { bufnr = bufnr }) then
+	if client:supports_method("textDocument/codeLens", { bufnr = bufnr }) then
 		vim.lsp.codelens.enable(true, { bufnr = bufnr })
 		vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave" }, {
 			buffer = bufnr,
