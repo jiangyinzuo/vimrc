fbba97ec357443da657f355d7642f23215b203d0
Yinzuo Jiang <jiangyinzuo@foxmail.com>
2026-04-22 21:47:49 +0800

    feat: mo

--- root/.config/nvim/lua/plugins/wsl.lua
+++ root/.config/nvim/lua/plugins/wsl.lua
@@ -1,14 +1,19 @@
 if vim.fn.has("wsl") == 1 then
 	return {
+		-- {
+		-- 	"iamcco/markdown-preview.nvim",
+		-- 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		-- 	-- or "cd app && yarn install"
+		-- 	build = "cd app && npx --yes yarn install",
+		-- 	init = function()
+		-- 		vim.g.mkdp_filetypes = { "markdown", "quarto" }
+		-- 	end,
+		-- 	ft = { "markdown", "quarto" },
+		-- },
 		{
-			"iamcco/markdown-preview.nvim",
-			cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-			-- or "cd app && yarn install"
-			build = "cd app && npx --yes yarn install",
-			init = function()
-				vim.g.mkdp_filetypes = { "markdown", "quarto" }
-			end,
-			ft = { "markdown", "quarto" },
+			"okm321/mo.nvim",
+			ft = { "markdown" },
+			opts = {},
 		},
 		{
 			"brianhuster/live-preview.nvim",
