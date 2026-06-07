ce532e84fe69bbc05a0ffefcf4b46652edcd777a
Yinzuo Jiang <jiangyinzuo@foxmail.com>
2026-06-07 20:57:36 +0800

    chore(wsl): remove scripts

--- wsl/execute_command.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void execute_command(char *command);
--- wsl/sumarapdf_no_terminal.c
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2024 Yinzuo Jiang
- */
-#include "execute_command.h"
-
-int main(int argc, char** argv) {
-  if (argc >= 2) execute_command(argv[1]);
-  return 0;
-}
