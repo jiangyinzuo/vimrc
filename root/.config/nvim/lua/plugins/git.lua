a65aa6b43a70136a4469db46e71f4f94d2a4b3a2
Yinzuo Jiang <jiangyinzuo@foxmail.com>
2026-09-23 03:21:57 +0000

    更新 git.lua

--- root/.config/nvim/lua/plugins/git.lua
+++ root/.config/nvim/lua/plugins/git.lua
@@ -15,17 +15,7 @@ return {
 		},
 	},
 	{
-		"akinsho/git-conflict.nvim",
-		version = "*",
-		config = function()
-			require("git-conflict").setup()
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "GitConflictDetected",
-				callback = function()
-					vim.notify("Conflict detected in " .. vim.fn.expand("<afile>"))
-				end,
-			})
-		end,
+		"tronikelis/conflict-marker.nvim",
 	},
 	{
 		"aaronhallaert/advanced-git-search.nvim",
